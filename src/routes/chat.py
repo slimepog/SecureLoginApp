e966ef3f59db2cc27e09fbdfa533e4f6298e978c
slimepog <liavshaked08@gmail.com>
2026-01-12 21:37:20 +0200

    Update comments in chat.py

--- src/routes/chat.py
+++ src/routes/chat.py
@@ -5,18 +5,21 @@ from models.message import send_message
 from utils.validators import validate_content
 from models.user import get_user_by_id
 from utils.sessions import require_single_session
+
+# sets the bp for all chat routes
 chat_bp = Blueprint("messages", __name__)
 
 
-
+# keeps track of online suers
 online_users = {}
 
-
+# handles initial connection to the socket
 @sockio.on("connect")
 @require_single_session   
 def handle_connect():
     print("Connection established baby", request.sid)
 
+# handles socket closing + removes user from online users
 @sockio.on("disconnect")
 @require_single_session   
 def handle_disconnect():
@@ -24,6 +27,7 @@ def handle_disconnect():
     username = online_users.pop(sid, None)
     print("Brudda disconnected", sid, username)
 
+# handles joining the chat
 @sockio.on("join")
 @require_single_session   
 def handle_join(data):
@@ -33,23 +37,24 @@ def handle_join(data):
 
     emit("system", {"msg": f"{username} joined the chat"}, broadcast=True)  
 
+# handles messages (in main chat - I think)
 @sockio.on("message")
 @require_single_session   
 def handle_message(data):
 
     try: 
-        validate_content(data['content'])
+        validate_content(data['content']) # checks content is not empty
     except Exception as e:
         flash(str(e), "error")
         return
-    send_message(data)
+    send_message(data) # saves the message
     sender = get_user_by_id(data["sender_id"])
 
     sender_username = sender["username"]
     data_sent = {"sender": sender_username,    "content": data['content']}
     emit("chat", data_sent, broadcast=True)
 
-    
+# handles private messages - (not in use now)
 @sockio.on("private_message")
 @require_single_session   
 def handle_pm(data):
