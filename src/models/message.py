4d41ff297eb63d7f0f5860a72e90024956c40706
slimepog <liavshaked08@gmail.com>
2026-01-12 21:15:44 +0200

    Update comments in message.py

--- src/models/message.py
+++ src/models/message.py
@@ -2,8 +2,18 @@ from exceptions import EmptyMessage
 from utils.database import get_db
 from utils.validators import validate_content
 
+# --------NOTE--------
+# for now it is one big chat
+# add personalized vhat with specific people
+# --------NOTE--------
 
 
+
+
+# updates the messages db
+# NOTE --->>> fix this
+# update the recevier id from 1 
+# change this logic to better one
 def send_message(data):
     sender_id = data['sender_id']
     content = data['content']
@@ -16,36 +26,12 @@ def send_message(data):
     cursor.close()
     conn.close()
 
+# gets all messages from the db
+# only gets the username and content
 def get_all_messages():
     conn = get_db()
     cursor = conn.cursor()
     cursor.execute("SELECT messages.content, users.username FROM messages JOIN users ON messages.sender_id = users.id")
     messages = cursor.fetchall()
     return messages
-#  -------- for now it is one big chat ----------
-# def send_message(data):
-#     content = data['content']
 
-#     sender_id = data['sender_id']
-#     recv_id = data['recv_id']
-#     connection = get_db()
-#     cursor = connection.cursor()
-#     cursor.execute("INSERT INTO messages (sender_id, receiver_id, content) VALUES (?, ?, ?)", (
-#         sender_id, recv_id, content
-#     ))
-#     connection.commit()
-#     cursor.close()
-#     connection.close()
-
-# def get_messages_by_id(sender_id, recv_id):
-#     connecton = get_db()
-#     cursor = connecton.cursor()
-#     cursor.execute(
-#         "SELECT content, created_at FROM messages WHERE sender_id=? AND receiver_id=? ORDER BY created_at ASC",
-#         (sender_id, recv_id)
-#     )
-#     messages = cursor.fetchall()
-#     cursor.close()
-#     connecton.close()
-    
-#     return messages
