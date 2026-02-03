5eb684661e5b6dae9569dade85e8840645226131
slimepog <liavshaked08@gmail.com>
2026-02-03 18:29:28 +0200

    Bug fixes

--- src/config.py
+++ src/config.py
@@ -3,6 +3,6 @@ import os
 # config
 class Config:
     SECRET_KEY = os.urandom(32)
-    DB_PATH = "data/general.db"
+    DB_PATH = "../data/general.db"
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SECURE = True
