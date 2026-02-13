a57c2725ae95016d521a38412558958773ce71f2
slimepog <liavshaked08@gmail.com>
2026-02-13 21:02:58 +0200

    added/fixed hsts

--- src/app.py
+++ src/app.py
@@ -37,6 +37,10 @@ def create_app():
 
     Talisman(
         app,
+        force_https=True,  # Redirect HTTP to HTTPS
+        strict_transport_security=True,
+        strict_transport_security_max_age=31536000,  # 1 year
+        strict_transport_security_include_subdomains=True,
         referrer_policy='strict-origin-when-cross-origin',
         content_security_policy=csp,
         content_security_policy_nonce_in=['script-src']
